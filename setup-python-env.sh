9c488dec96310cdc9b69ceb34015c4802fe19c03
romaintailhurat <romain.tailhurat@agriculture.gouv.fr>
2021-07-21 15:13:21 +0000

    Python setup script, pyenv install still problematic

--- setup-python-env.sh
+++ setup-python-env.sh
@@ -1,6 +1,10 @@
-# we take for granted the installation of pyenv
-pyenv install 3.9.6
+# We take for granted the installation of pyenv
+# We need to provide here the uncommon installation path of openssl
+# https://github.com/pyenv/pyenv/wiki/Common-build-problems#error-the-python-ssl-extension-was-not-compiled-missing-the-openssl-lib
+#
+# --------------------
+# WARNING still problematic
+# --------------------
+export CONFIGURE_OPTS="--with-openssl=$(which openssl)"
+pyenv install -v 3.9.6
 pyenv local 3.9.6
-
-# pytest
-pip install pytest
