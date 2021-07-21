15cfd3b867062e9ea4260399ea9233d73d167cf2
romaintailhurat <romain.tailhurat@agriculture.gouv.fr>
2021-07-21 15:55:33 +0000

    Experimenting with splinter UI tests

---
+++ test/test_home_page_display.py
@@ -0,0 +1,23 @@
+from splinter import Browser
+from selenium import webdriver
+import pytest
+from src.HomePage import HomePage
+
+@pytest.fixture
+def suite_browser():
+    # Setting up Chrome Options in order to have it run in a container
+    opts = webdriver.ChromeOptions()
+    opts.add_argument("--headless")
+    opts.add_argument("--no-sandbox")
+    opts.add_argument("--disable-gpu")
+    opts.add_argument("--disable-extensions")
+    browser = Browser("chrome", options=opts)
+
+    return browser
+
+def test_title_is_ok(suite_browser):
+    hp = HomePage(browser = suite_browser)
+    hp.browser.visit("http://www.insee.fr")    
+    print(hp.browser.url)
+    #hp.browser.html
+    assert False
