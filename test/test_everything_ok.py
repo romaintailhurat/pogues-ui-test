804e82e2bb2eff087a6144a14edd2637b01630d9
romaintailhurat <romain.tailhurat@agriculture.gouv.fr>
2021-07-21 15:18:35 +0000

    First use of pytest.fixture annotation

--- test/test_everything_ok.py
+++ test/test_everything_ok.py
@@ -4,9 +4,11 @@ is ok.
 """
 from splinter import Browser
 from selenium import webdriver
+import pytest
 from src.HomePage import HomePage
 
-def test_ok():
+@pytest.fixture
+def suite_browser():
     # Setting up Chrome Options in order to have it run in a container
     opts = webdriver.ChromeOptions()
     opts.add_argument("--headless")
@@ -15,5 +17,8 @@ def test_ok():
     opts.add_argument("--disable-extensions")
     browser = Browser("chrome", options=opts)
 
-    hp = HomePage(browser = browser)
+    return browser
+
+def test_ok(suite_browser):
+    hp = HomePage(browser = suite_browser)
     assert hp.url != "https://www.insee.fr"
